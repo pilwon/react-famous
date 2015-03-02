7fd5d368b39bdbbbf99ed038b074f0ee397cb3c7
Pilwon Huh <pilwon@gmail.com>
2015-03-02 17:28:11 -0500

    Improved HeaderFooterLayout example with use of modifier.

--- examples/HeaderFooterLayout/components/App.jsx
+++ examples/HeaderFooterLayout/components/App.jsx
@@ -1,30 +1,39 @@
 import React from 'react';
 
 import Context from 'react-famous/core/Context';
+import Modifier from 'react-famous/core/Modifier';
 import Surface from 'react-famous/core/Surface';
+import Transform from 'react-famous/core/Transform';
 import HeaderFooterLayout from 'react-famous/views/HeaderFooterLayout';
 
 export default React.createClass({
   render() {
     return (
       <Context>
-        <HeaderFooterLayout headerSize={50} footerSize={20}>
-          <HeaderFooterLayout.Header>
-            <Surface properties={{backgroundColor: '#4B4F53', color: 'white'}}>
-              Header
-            </Surface>
-          </HeaderFooterLayout.Header>
-          <HeaderFooterLayout.Content>
-            <Surface properties={{backgroundColor: '#131416', color: 'white'}}>
-              Content
-            </Surface>
-          </HeaderFooterLayout.Content>
-          <HeaderFooterLayout.Footer>
-            <Surface properties={{backgroundColor: '#26292C', color: 'white'}}>
-              Footer
-            </Surface>
-          </HeaderFooterLayout.Footer>
-        </HeaderFooterLayout>
+        <Modifier align={[.5, .5]}
+                  origin={[.5, .5]}
+                  size={[300, 300]}
+                  transform={Transform.rotateZ(.2)}>
+          <HeaderFooterLayout headerSize={100} footerSize={50}>
+            <HeaderFooterLayout.Header>
+              <Surface properties={{backgroundColor: '#ff8888', lineHeight: '100px', textAlign: 'center'}}
+                       size={[undefined, 100]}>
+                Header
+              </Surface>
+            </HeaderFooterLayout.Header>
+            <HeaderFooterLayout.Content>
+              <Surface properties={{backgroundColor: '#88ff88', lineHeight: '150px', textAlign: 'center'}}>
+                Content
+              </Surface>
+            </HeaderFooterLayout.Content>
+            <HeaderFooterLayout.Footer>
+              <Surface properties={{backgroundColor: '#8888ff', lineHeight: '50px', textAlign: 'center'}}
+                       size={[undefined, 50]}>
+                Footer
+              </Surface>
+            </HeaderFooterLayout.Footer>
+          </HeaderFooterLayout>
+        </Modifier>
       </Context>
     );
   }
