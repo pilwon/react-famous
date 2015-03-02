4af1549aa808c035aecf8f5986d8c616c6fe06c1
Pilwon Huh <pilwon@gmail.com>
2015-03-02 17:35:54 -0500

    Changed example names.

--- examples/TestHelloWorld/components/App.jsx
+++ examples/TestHelloWorld/components/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Context from 'react-famous/core/Context';
-
-import CommentBox from './CommentBox';
+import Modifier from 'react-famous/core/Modifier';
+import Surface from 'react-famous/core/Surface';
 
 export default React.createClass({
   render() {
     return (
       <Context>
-        <CommentBox/>
+        <Modifier align={[0.5, 0.5]} origin={[0.5, 0.5]}>
+          <Surface size={[150, 100]} properties={{backgroundColor: 'red', color: '#fff', textAlign: 'center', lineHeight: '100px'}}>
+            Hello World
+          </Surface>
+        </Modifier>
       </Context>
     );
   }
