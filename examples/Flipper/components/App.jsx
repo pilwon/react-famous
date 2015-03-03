7869decb38e2b29528482d8722ea7760e142e981
Pilwon Huh <pilwon@gmail.com>
2015-03-03 06:04:53 -0500

    Added Flipper.

---
+++ examples/Flipper/components/App.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+import Context from 'react-famous/core/Context';
+import Engine from 'react-famous/core/Engine';
+import Modifier from 'react-famous/core/Modifier';
+import Surface from 'react-famous/core/Surface';
+import Flipper from 'react-famous/views/Flipper';
+
+export default React.createClass({
+  onFlipperReady() {
+    let toggle = false;
+
+    Engine.on('click', () => {
+      let angle = toggle ? 0 : Math.PI;
+
+      this.refs.flipper.famous.setAngle(angle, {
+        curve: 'easeOutBounce',
+        duration: 500
+      });
+
+      toggle = !toggle;
+    });
+  },
+
+  render() {
+    let frontOptions = {
+      properties: {
+        backgroundColor: 'red',
+        color: 'white',
+        lineHeight: '200px',
+        textAlign: 'center'
+      },
+      size: [200, 200]
+    };
+
+    let backOptions = {
+      properties: {
+        backgroundColor: 'blue',
+        color: 'white',
+        lineHeight: '200px',
+        textAlign: 'center'
+      }, 
+      size: [200, 200]
+    };
+
+    return (
+      <Context perspective={500}>
+        <Surface options={{properties: {lineHeight: '100px', textAlign: 'center'}, size: [undefined, 100]}}>
+          Click anywhere on the screen.
+        </Surface>
+        <Modifier options={{align: [.5, .5], origin: [.5, .5]}}>
+          <Flipper ref="flipper" onReady={this.onFlipperReady} options={{headerSize: 100, footerSize: 50}}>
+            <Flipper.Front>
+              <Surface options={frontOptions}>
+                front
+              </Surface>
+            </Flipper.Front>
+            <Flipper.Back>
+              <Surface options={backOptions}>
+                back
+              </Surface>
+            </Flipper.Back>
+          </Flipper>
+        </Modifier>
+      </Context>
+    );
+  }
+});
---
+++ src/views/Flipper.jsx
@@ -0,0 +1,95 @@
+import RenderNode from 'famous/core/RenderNode';
+import Flipper from 'famous/views/Flipper';
+import React from 'react';
+import cloneWithProps from 'react/lib/cloneWithProps';
+
+import FamousMixin from '../lib/FamousMixin';
+
+let Component = React.createClass({
+  mixins: [FamousMixin],
+
+  famousCreate() {
+    let flipper = new Flipper(this.props.options);
+    this.setFamous(flipper);
+    this.setFamousNode(this.getFamousParentNode().add(flipper));
+
+    let backNode = new RenderNode();
+    let frontNode = new RenderNode();
+    flipper.setBack(backNode);
+    flipper.setFront(frontNode);
+
+    this.setFamousKeyedNodes({
+      back: backNode,
+      front: frontNode
+    });
+  },
+
+  famousUpdate(nextProps) {
+    let flipper = this.getFamous();
+
+    flipper.setOptions(nextProps.options);
+  },
+
+  render() {
+    if (!this.getFamousReady()) { return null; }
+
+    let children = this.props.children.map((child) => {
+      switch (child.type) {
+        case Component.Back:
+          return cloneWithProps(child, {key: 'back'});
+        case Component.Front:
+          return cloneWithProps(child, {key: 'front'});
+        default:
+          return null;
+      }
+    });
+
+    return (
+      <div data-famous="Flipper">
+        {children}
+      </div>
+    );
+  }
+});
+
+Component.Back = React.createClass({
+  mixins: [FamousMixin],
+
+  famousCreate() {
+    let renderNode = new RenderNode();
+    this.setFamous(renderNode);
+    this.setFamousNode(this.getFamousParentNode().add(renderNode));
+  },
+
+  render() {
+    if (!this.getFamousReady()) { return null; }
+
+    return (
+      <div data-famous="Flipper.Back">
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+Component.Front = React.createClass({
+  mixins: [FamousMixin],
+
+  famousCreate() {
+    let renderNode = new RenderNode();
+    this.setFamous(renderNode);
+    this.setFamousNode(this.getFamousParentNode().add(renderNode));
+  },
+
+  render() {
+    if (!this.getFamousReady()) { return null; }
+
+    return (
+      <div data-famous="Flipper.Front">
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+export default Component;
