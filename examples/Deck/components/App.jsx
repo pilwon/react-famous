d1429f8d7a4e3b9a950c055cfa98a39cd9f2cdd0
Pilwon Huh <pilwon@gmail.com>
2015-03-03 02:30:54 -0500

    Every famo.us wrapper component supports special _onReady callback prop.

--- examples/Deck/components/App.jsx
+++ examples/Deck/components/App.jsx
@@ -10,29 +10,32 @@ import Deck from 'react-famous/views/Deck';
 Transitionable.registerMethod('spring', SpringTransition);
 
 export default React.createClass({
-  componentDidMount() {
-    Timer.setInterval(() => {
-      this.refs.deck.toggle();
-    }, 500);
+  onReady() {
+    this.refs.deck.toggle();
   },
 
   render() {
     let surfaces = [];
+    let transition = {
+      method: 'spring',
+      period: 300,
+      dampingRatio: 0.5
+    };
 
     for (let i = 0; i < 5; i++) {
+      let backgroundColor = 'hsla(' + ((i * 5 + i) * 15 % 360) + ', 60%, 50%, 0.8)';
       let surface = (
-        <Surface key={i} properties={{backgroundColor: 'hsla(' + ((i * 5 + i) * 15 % 360) + ', 60%, 50%, 0.8)'}} size={[100, 200]}>
+        <Surface key={i} properties={{backgroundColor}} size={[100, 200]}>
           {i}
         </Surface>
       );
-
       surfaces.push(surface);
     }
 
     return (
       <Context>
         <Modifier align={[0.5, 0.5]} origin={[0.5, 0.5]}>
-          <Deck ref="deck" itemSpacing={10} transition={{method: 'spring', period: 300, dampingRatio: 0.5}} stackRotation={0.02}>
+          <Deck itemSpacing={10} transition={transition} stackRotation={0.02} ref="deck" _onReady={this.onReady}>
             {surfaces}
           </Deck>
         </Modifier>
--- src/lib/FamousMixin.js
+++ src/lib/FamousMixin.js
@@ -1,10 +1,16 @@
 import isFunction from 'lodash/lang/isFunction';
+import React from 'react';
 import PureRenderMixin from 'react/lib/ReactComponentWithPureRenderMixin';
 
 import FamousNodeMixin from './FamousNodeMixin';
+import FamousUtil from './FamousUtil';
 
 export default {
   mixins: [FamousNodeMixin, PureRenderMixin],
+
+  propTypes: {
+    _onReady: React.PropTypes.func
+  },
 
   componentWillMount() {
     this.setFamousReady(false);
@@ -12,15 +18,19 @@ export default {
 
   componentDidMount() {
     if (isFunction(this.famousCreate)) {
-      this.famousCreate(this.props);
+      this.famousCreate(FamousUtil.sanitizeProps(this.props));
     }
     this.setFamousReady(true);
-    this.forceUpdate();
+    this.forceUpdate(() => {
+      if (this.props._onReady) {
+        this.props._onReady();
+      }
+    });
   },
 
   componentWillReceiveProps(nextProps) {
     if (isFunction(this.famousUpdate)) {
-      this.famousUpdate(nextProps);
+      this.famousUpdate(FamousUtil.sanitizeProps(nextProps));
     }
   },
 
