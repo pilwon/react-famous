748a3c0e5cde8575382ac11384dee8ac71abb71e
Pilwon Huh <pilwon@gmail.com>
2015-03-05 04:18:35 -0500

    Expose all of the surface's event callbacks as function type props.

--- src/core/Surface.jsx
+++ src/core/Surface.jsx
@@ -5,12 +5,40 @@ import React from 'react';
 import FamousMixin from '../lib/FamousMixin';
 import FamousUtil from '../lib/FamousUtil';
 
+const SURFACE_EVENTS = [
+  {prop: 'onClick', type: 'click'},
+  {prop: 'onKeyDown', type: 'keydown'},
+  {prop: 'onKeyPress', type: 'keypress'},
+  {prop: 'onKeyUp', type: 'keyup'},
+  {prop: 'onMouseDown', type: 'mousedown'},
+  {prop: 'onMouseMove', type: 'mousemove'},
+  {prop: 'onMouseOut', type: 'mouseout'},
+  {prop: 'onMouseOver', type: 'mouseover'},
+  {prop: 'onMouseUp', type: 'mouseup'},
+  {prop: 'onTouchCancel', type: 'touchcancel'},
+  {prop: 'onTouchEnd', type: 'touchend'},
+  {prop: 'onTouchMove', type: 'touchmove'},
+  {prop: 'onTouchStart', type: 'touchstart'}
+];
+
 export default React.createClass({
   mixins: [FamousMixin],
 
   propTypes: {
     eventKey: React.PropTypes.any,
-    onClick: React.PropTypes.func
+    onClick: React.PropTypes.func,
+    onKeyDown: React.PropTypes.func,
+    onKeyPress: React.PropTypes.func,
+    onKeyUp: React.PropTypes.func,
+    onMouseDown: React.PropTypes.func,
+    onMouseMove: React.PropTypes.func,
+    onMouseOut: React.PropTypes.func,
+    onMouseOver: React.PropTypes.func,
+    onMouseUp: React.PropTypes.func,
+    onTouchCancel: React.PropTypes.func,
+    onTouchEnd: React.PropTypes.func,
+    onTouchMove: React.PropTypes.func,
+    onTouchStart: React.PropTypes.func
   },
 
   famousCreate() {
@@ -18,11 +46,13 @@ export default React.createClass({
     this.setFamous(surface);
     this.setFamousNode(this.getFamousParentNode().add(surface));
 
-    if (this.props.onClick) {
-      surface.on('click', () => {
-        this.props.onClick(this.props.eventKey);
-      });
-    }
+    SURFACE_EVENTS.forEach((event) => {
+      if (this.props[event.prop]) {
+        surface.on(event.type, () => {
+          this.props[event.prop](this.props.eventKey);
+        });
+      }
+    });
 
     if (!isUndefined(this.props.children)) {
       surface.setContent(FamousUtil.renderContent(this.props.children));
