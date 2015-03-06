64dadbe9f433410415476b6c1bfc50e84f66c665
Pilwon Huh <pilwon@gmail.com>
2015-03-05 23:54:55 -0500

    Added description in surfaces/ImageSurface example.

--- examples/surfaces/ImageSurface/components/App.jsx
+++ examples/surfaces/ImageSurface/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Context from 'react-famous/core/Context';
 import Modifier from 'react-famous/core/Modifier';
+import Surface from 'react-famous/core/Surface';
 import ImageSurface from 'react-famous/surfaces/ImageSurface';
 
 const IMAGE_URL = 'http://code.famo.us/assets/famous.jpg';
@@ -9,6 +10,9 @@ export default React.createClass({
   render() {
     return (
       <Context>
+        <Surface options={{properties: {lineHeight: '100px', textAlign: 'center'}, size: [undefined, 100]}}>
+          The image is created with ImageSurface.
+        </Surface>
         <Modifier options={{align: [0.5, 0.5], origin: [0.5, 0.5]}}>
           <ImageSurface options={{content: IMAGE_URL, size: [200, 200]}}/>
         </Modifier>
