fef882c6373e245cc9e70b1dad205dd83dd1d0c1
Pilwon Huh <pilwon@gmail.com>
2015-03-09 09:00:58 -0400

    Created style.css for example.

--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,7 @@
-import 'famous/core/famous.css';
 import React from 'react';
 
+import 'famous/core/famous.css';
+import './style.css';
 import App from './components/App';
 
 React.render(React.createElement(App), document.body);
