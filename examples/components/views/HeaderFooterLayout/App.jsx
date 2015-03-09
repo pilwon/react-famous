6c68f7ad86666cbd6615fb16745fb676a66222a2
Pilwon Huh <pilwon@gmail.com>
2015-03-09 11:47:56 -0400

    Updated HeaderFooterLayout example.

--- examples/components/views/HeaderFooterLayout/App.jsx
+++ examples/components/views/HeaderFooterLayout/App.jsx
@@ -1,15 +1,15 @@
 import Transform from 'famous/core/Transform';
 import React from 'react';
 import Context from 'react-famous/core/Context';
+import Modifier from 'react-famous/core/Modifier';
 import Surface from 'react-famous/core/Surface';
-import StateModifier from 'react-famous/modifiers/StateModifier';
 import HeaderFooterLayout from 'react-famous/views/HeaderFooterLayout';
 
 export default class extends React.Component {
   render() {
     return (
       <Context>
-        <StateModifier options={{align: [.5, .5], origin: [.5, .5], size: [300, 300], transform: Transform.rotateZ(.2)}}>
+        <Modifier options={{align: [.5, .5], origin: [.5, .5], size: [300, 300], transform: Transform.rotateZ(.2)}}>
           <HeaderFooterLayout options={{headerSize: 100, footerSize: 50}}>
             <HeaderFooterLayout.Header>
               <Surface options={{properties: {backgroundColor: '#ff8888', lineHeight: '100px', textAlign: 'center'}, size: [undefined, 100]}}>
@@ -27,7 +27,7 @@ export default class extends React.Component {
               </Surface>
             </HeaderFooterLayout.Footer>
           </HeaderFooterLayout>
-        </StateModifier>
+        </Modifier>
       </Context>
     );
   }
