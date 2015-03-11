c3ec4666e484786a2d57b181e639dfe69e7f108a
Pilwon Huh <pilwon@gmail.com>
2015-03-11 16:07:40 -0400

    Refactored Surface's event callback props code. Created FamousConstants.

--- src/core/Surface.jsx
+++ src/core/Surface.jsx
@@ -4,29 +4,14 @@ import defaults from 'lodash/object/defaults';
 import React from 'react';
 
 import FamousComponent from '../lib/FamousComponent';
+import FamousConstants from '../lib/FamousConstants';
 import FamousUtil from '../lib/FamousUtil';
-
-const SURFACE_EVENTS = [
-  {prop: 'onClick', type: 'click'},
-  {prop: 'onKeyDown', type: 'keydown'},
-  {prop: 'onKeyPress', type: 'keypress'},
-  {prop: 'onKeyUp', type: 'keyup'},
-  {prop: 'onMouseDown', type: 'mousedown'},
-  {prop: 'onMouseMove', type: 'mousemove'},
-  {prop: 'onMouseOut', type: 'mouseout'},
-  {prop: 'onMouseOver', type: 'mouseover'},
-  {prop: 'onMouseUp', type: 'mouseup'},
-  {prop: 'onTouchCancel', type: 'touchcancel'},
-  {prop: 'onTouchEnd', type: 'touchend'},
-  {prop: 'onTouchMove', type: 'touchmove'},
-  {prop: 'onTouchStart', type: 'touchstart'}
-];
 
 class Surface extends FamousComponent {
   famousCreate() {
     let surface = new FamousSurface(this.props.options);
 
-    SURFACE_EVENTS.forEach((event) => {
+    FamousConstants.SURFACE_EVENTS.forEach((event) => {
       if (this.props[event.prop]) {
         surface.on(event.type, () => {
           this.props[event.prop](this.props.eventKey);
@@ -63,21 +48,6 @@ class Surface extends FamousComponent {
 
 defaults(Surface, FamousSurface);
 
-Surface.propTypes = {
-  eventKey: React.PropTypes.any,
-  onClick: React.PropTypes.func,
-  onKeyDown: React.PropTypes.func,
-  onKeyPress: React.PropTypes.func,
-  onKeyUp: React.PropTypes.func,
-  onMouseDown: React.PropTypes.func,
-  onMouseMove: React.PropTypes.func,
-  onMouseOut: React.PropTypes.func,
-  onMouseOver: React.PropTypes.func,
-  onMouseUp: React.PropTypes.func,
-  onTouchCancel: React.PropTypes.func,
-  onTouchEnd: React.PropTypes.func,
-  onTouchMove: React.PropTypes.func,
-  onTouchStart: React.PropTypes.func
-};
+Surface.propTypes = defaults({}, FamousConstants.SURFACE_PROPTYPES);
 
 export default Surface;
