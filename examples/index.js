617a8444253ea47b07c5ff06e96180747727e0a7
Pilwon Huh <pilwon@gmail.com>
2015-03-11 20:11:20 -0400

    Updated examples/index.js

--- examples/index.js
+++ examples/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 
 import 'famous/core/famous.css';
+import './lib/overscroll';
 import './styles/index.less';
 import App from './components/App';
-
-import './lib/overscroll';
-// window.addEventListener('touchmove', (event) => event.preventDefault(), true);
 
 React.render(React.createElement(App), document.body);
