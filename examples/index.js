f85328c8b8995aac7162d536919a185780c54a58
Pilwon Huh <pilwon@gmail.com>
2015-03-20 14:39:06 -0400

    Added FastClik. Reorganized index.js

--- examples/index.js
+++ examples/index.js
@@ -1,8 +1,10 @@
+import './styles/index.less';
+
+import 'famous/inputs/FastClick';
+import './lib/overscroll';
+
 import React from 'react';
 
-import 'famous/core/famous.css';
-import './lib/overscroll';
-import './styles/index.less';
 import App from './components/App';
 
 React.render(React.createElement(App), document.body);
